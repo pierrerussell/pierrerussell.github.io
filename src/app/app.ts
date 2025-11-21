3154f699e65d2e7fc05eedc9d0ac9f663ff0a941
pierre <pierrerussellhojun@gmail.com>
2025-11-22 07:37:51 +0800

    Add Google Analytics

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet ,Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('MyProjects');
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      gtag('event', 'page_view', {
+        page_path: event.urlAfterRedirects,
+      });
+    });
+  }
 }
