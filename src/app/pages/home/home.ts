51525d887197e22247a32fc3c2dfb37dea9e4560
pierre <pierrerussellhojun@gmail.com>
2025-11-24 07:24:51 +0800

    Add Google Analytics service and track button clicks on home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -5,6 +5,7 @@ import { CodeBlockComponent } from '../../shared/components/code-block/code-bloc
 import { ProjectPreviewComponent } from '../../shared/components/project-preview/project-preview';
 import { Project } from '../../core/models/project.model';
 import { ProjectsData } from '../../core/data/projects.data';
+import { GoogleAnalyticsService } from '../../core/services/google-analytics.service';
 
 @Component({
   selector: 'app-home',
@@ -14,4 +15,10 @@ import { ProjectsData } from '../../core/data/projects.data';
 })
 export class HomeComponent {
   featuredProjects: Project[] = ProjectsData.getFeaturedProjects();
+
+  constructor(private ga: GoogleAnalyticsService) {}
+
+  trackButtonClick(buttonName: string) {
+    this.ga.buttonClick(buttonName, 'home_hero');
+  }
 }
